4d01af7d2ab3a3075f2ff5faa19c03d71fe4ebb5
impactlearner <time.darden@gmail.com>
2020-05-02 21:24:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,8 @@ end
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  id_hash_generator(4) = {
-    :id => 4
+  #id_hash_generator(number) = {
+  id_hash_generator[:id] = number
+  #  :id => number
   }
 end
