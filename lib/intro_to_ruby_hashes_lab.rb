66177706f10a85404aab0ae2c179310f0a714617
impactlearner <time.darden@gmail.com>
2020-05-02 21:10:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,11 +15,12 @@ def pioneer
   pioneer = {
     :name => 'Grace Hopper'
   }
-
-
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  id_hash_generator(number) = {
+    :id = > number
+  }
 end
